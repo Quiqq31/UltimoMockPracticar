350434c162d5691bd19c8fd8ecd88ded734255cd
Enrique Villar <9202686@alumnos.ufv.es>
2025-01-16 18:11:23 +0100

    Backend changes . Implement vehicle deletion functionality and add sample vehicle data

--- Backend2/src/main/java/com/example/Backend2/VehicleController.java
+++ Backend2/src/main/java/com/example/Backend2/VehicleController.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,5 +28,25 @@ public class VehicleController {
         return newVehicle;
     }
 
-    
+    @PostMapping("/vehicles/{licensePlate}")
+    public String deletVehicle(@PathVariable String licensePlate) throws IOException {
+        JsonManager manager = new JsonManager();
+        ArrayList<Vehicle> vehiclesList = manager.getVehicles();
+        ArrayList<Vehicle> newVehicles = new ArrayList<Vehicle>();
+        ArrayList<Vehicle> deletedVehicles = new ArrayList<Vehicle>();
+
+        for (Vehicle vehicleA : vehiclesList) {
+            if(!vehicleA.getLicensePlate().equals(licensePlate)){
+                newVehicles.add(vehicleA); // Add the vehicle to the newVehicles list
+            } else {
+                deletedVehicles.add(vehicleA); // Add the deleted vehicle to the deletedVehicles list
+            }
+        }
+
+        manager.saveVehicles(newVehicles);
+        
+        return new String("Vehicle deleted successfully");
+    }
+
+
 }
